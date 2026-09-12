9b737bcfc6a154211c63aea6574a9ba9eb545d89
Razak.ck <69842830+razakck@users.noreply.github.com>
2026-09-12 18:31:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,13 @@ $conn = new mysqli($servername, $username, $password, $db,3306);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+ 
+    throw new RuntimeException(
+        'Database connection failed: ' . $connection->connect_error
+    );
+}
+   
+  
 }
 if(isset($_POST['firstname']) && isset($_POST['email'])){
 $sql = "INSERT INTO data (firstname,email)
